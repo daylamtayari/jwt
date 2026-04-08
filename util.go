9bcb032a4969860b4e1f92b99145e22485164151
Daylam Tayari <git@tayari.gg>
2026-04-07 23:10:11 -0500

    Added stdin, printing, and fatal util funcs

---
+++ util.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// Exits the program fatally and prints the given error
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+	os.Exit(1)
+}
+
+// Marshals to JSON and prints the provided value
+func printJSON(v any) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		fatal(err)
+	}
+	fmt.Printf("%s\n", string(data))
+}
+
+// Returns true if a value was passed throught stdin
+func hasStdin() bool {
+	siStat, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+	return (siStat.Mode()&os.ModeCharDevice == 0)
+}
+
+// Reads the value in from stdin
+func readStdin() string {
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fatal(err)
+	}
+	return strings.TrimSpace(string(data))
+}
